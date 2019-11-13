a515340318c53ee448e473c7299ed6cc61fc9e17
anuj <anujshah584@gmail.com>
2019-11-13 15:58:11 +0530

    revert: example

--- packages/searchbase/examples/with-react/src/index.js
+++ packages/searchbase/examples/with-react/src/index.js
@@ -7,27 +7,26 @@ import Searchbase from '@appbaseio/searchbase';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    const index = 'good-book-ds-latest';
+    const index = 'gitxplore-latest-app';
     const url = 'https://scalr.api.appbase.io';
-    const credentials = 'IPM14ICqp:8e573e86-8802-4a27-a7a1-4c7d0c62c186';
+    const credentials = 'LsxvulCKp:a500b460-73ff-4882-8d34-9df8064b3b38';
 
     this.searchBase = new Searchbase({
       index,
       url,
-      dataField: 'original_title',
-      aggregationField: 'original_title.keyword',
+      dataField: [
+        'name',
+        'description',
+        'name.raw',
+        'fullname',
+        'owner',
+        'topics'
+      ],
       credentials
     });
 
     // Pre-load results
     this.searchBase.triggerQuery();
-
-    this.searchBase.onSuggestions = (next, prev) => {
-      console.log('sugges', next, prev);
-    };
-    this.searchBase.onAggregationData = (next, prev) => {
-      console.log('on aggs data', next, prev);
-    };
 
     this.searchBase.subscribeToStateChanges(() => {
       this.forceUpdate();
