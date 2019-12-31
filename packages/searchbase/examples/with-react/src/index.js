352a2cea05e3cb1ab2c21470f10b6fbf3e86c07c
anuj <anujshah584@gmail.com>
2019-12-31 18:44:18 +0530

    Revert "example"

--- packages/searchbase/examples/with-react/src/index.js
+++ packages/searchbase/examples/with-react/src/index.js
@@ -7,16 +7,22 @@ import Searchbase from '@appbaseio/searchbase';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    const index = 'business';
-    const url = 'http://localhost:8000';
-    const credentials = 'foo:bar';
+    const index = 'gitxplore-latest-app';
+    const url = 'https://scalr.api.appbase.io';
+    const credentials = 'LsxvulCKp:a500b460-73ff-4882-8d34-9df8064b3b38';
 
     this.searchBase = new Searchbase({
       index,
       url,
-      dataField: ['authors', 'authors.raw'],
-      credentials,
-      analytics: true
+      dataField: [
+        'name',
+        'description',
+        'name.raw',
+        'fullname',
+        'owner',
+        'topics'
+      ],
+      credentials
     });
 
     // Pre-load results
@@ -40,7 +46,6 @@ class App extends React.Component {
   };
 
   render() {
-    console.log('searchbase', this.searchBase);
     return (
       <div className="App">
         <input
