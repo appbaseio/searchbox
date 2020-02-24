2517b4889001ba37e25021a159043898536d21ad
anuj <anujshah584@gmail.com>
2020-02-24 11:43:15 +0530

    build: v1.1.0-alpha.0

--- packages/vue-searchbox/rollup.config.js
+++ packages/vue-searchbox/rollup.config.js
@@ -74,7 +74,7 @@ export default {
       : {},
     umd
       ? commonjs({
-          include: ["node_modules/**"]
+          include: ["../../node_modules/**"]
         })
       : {},
     babel({
