a049d5933b436b8074ebb06ee71e674161cc6ff4
Siddharth Kothari <sids.aquarius@gmail.com>
2020-06-17 05:42:29 +0530

    content: update website content

--- src/pages/vue/Home.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import config from './../../constants/config/home/vue';
-import Home from './../../components/Home';
-import theme from './../../constants/theme/vue';
-
-export default () => <Home config={config} theme={theme} />;
