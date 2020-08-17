d822e92e25454a6dffb44844462a0838e292c658
jyash97 <jyash97@gmail.com>
2020-08-17 23:33:36 +0530

    fix(site): assests path

--- src/components/MobileMenu.js
+++ src/components/MobileMenu.js
@@ -36,9 +36,9 @@ const MobileMenu = ({ onToggle, visible }) => {
                   viewBox="0 0 24 24"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M6 18L18 6M6 6l12 12"
                   />
                 </svg>
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -5,32 +5,32 @@ import Button from './Button';
 const imageConfig = [
   {
     srcSet:
-      '../../searchbox/images/testimonials/dol/logo@1x.png 1x, ../../searchbox/images/testimonials/dol/logo@2x.png 2x,../../searchbox/images/testimonials/dol/logo@3x.png 3x',
+      '/images/testimonials/dol/logo@1x.png 1x, /images/testimonials/dol/logo@2x.png 2x,/images/testimonials/dol/logo@3x.png 3x',
     alt: 'US Department of Labor'
   },
   {
     srcSet:
-      '../../searchbox/images/testimonials/fbresearch/logo@1x.png 1x, ../../searchbox/images/testimonials/fbresearch/logo@2x.png 2x,../../searchbox/images/testimonials/fbresearch/logo@3x.png 3x',
+      '/images/testimonials/fbresearch/logo@1x.png 1x, /images/testimonials/fbresearch/logo@2x.png 2x,/images/testimonials/fbresearch/logo@3x.png 3x',
     alt: 'Facebook Research'
   },
   {
     srcSet:
-      '../../searchbox/images/testimonials/rumbleon/rumbleon@1x.png 1x, ../../searchbox/images/testimonials/rumbleon/rumbleon@2x.png 2x,../../searchbox/images/testimonials/rumbleon/rumbleon@3x.png 3x',
+      '/images/testimonials/rumbleon/rumbleon@1x.png 1x, /images/testimonials/rumbleon/rumbleon@2x.png 2x,/images/testimonials/rumbleon/rumbleon@3x.png 3x',
     alt: 'RumbleOn'
   },
   {
     srcSet:
-      '../../searchbox/images/testimonials/betagov/logo@1x.png 1x, ../../searchbox/images/testimonials/betagov/logo@2x.png 2x,../../searchbox/images/testimonials/betagov/logo@3x.png 3x',
+      '/images/testimonials/betagov/logo@1x.png 1x, /images/testimonials/betagov/logo@2x.png 2x,/images/testimonials/betagov/logo@3x.png 3x',
     alt: 'beta.gouv.fr'
   },
   {
     srcSet:
-      '../../searchbox/images/testimonials/nasa/Nasa@1x.png 1x, ../../searchbox/images/testimonials/nasa/Nasa@2x.png 2x,../../searchbox/images/testimonials/nasa/Nasa@3x.png 3x',
+      '/images/testimonials/nasa/Nasa@1x.png 1x, /images/testimonials/nasa/Nasa@2x.png 2x,/images/testimonials/nasa/Nasa@3x.png 3x',
     alt: 'Nasa'
   },
   {
     srcSet:
-      '../../searchbox/images/testimonials/reactioncommerce/logo@1x.png 1x, ../../searchbox/images/testimonials/reactioncommerce/logo@2x.png 2x,../../searchbox/images/testimonials/reactioncommerce/logo@3x.png 3x',
+      '/images/testimonials/reactioncommerce/logo@1x.png 1x, /images/testimonials/reactioncommerce/logo@2x.png 2x,/images/testimonials/reactioncommerce/logo@3x.png 3x',
     alt: 'ReactionCommerce'
   }
 ];
@@ -42,7 +42,7 @@ const Testimonials = () => (
   >
     <div className="mt-10 gap-8 sm:gap-0 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
       {imageConfig.map(image => (
-        <div className="col-span-1 h-24 flex justify-center items-center">
+        <div key={image.alt} className="col-span-1 h-24 flex justify-center items-center">
           <img className="h-100" {...image} />
         </div>
       ))}
