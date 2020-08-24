281a4e15d18bff98155dfbb79c80e52c8e1a87d6
jyash97 <jyash97@gmail.com>
2020-08-24 16:31:12 +0530

    fix: js example dataset, styles & add voice search

--- packages/react-searchbox/examples/demo/src/App.js
+++ packages/react-searchbox/examples/demo/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-
 import SearchBox from '@appbaseio/react-searchbox';
+
+import './styles.css';
 
 export default class App extends Component {
   render() {
--- packages/searchbox/examples/searchbar-with-style/index.js
+++ packages/searchbox/examples/searchbar-with-style/index.js
@@ -2,32 +2,54 @@ import searchbox from '@appbaseio/searchbox';
 import Searchbase from '@appbaseio/searchbase';
 
 const instance = new Searchbase({
-	index: 'gitxplore-latest-app',
-	credentials: 'LsxvulCKp:a500b460-73ff-4882-8d34-9df8064b3b38',
-	url: 'https://scalr.api.appbase.io',
-	size: 5,
-	dataField: [
-		'name',
-		'description',
-		'name.raw',
-		'fullname',
-		'owner',
-		'topics'
-	]
+  index: 'good-books-ds',
+  credentials: 'nY6NNTZZ6:27b76b9f-18ea-456c-bc5e-3a5263ebc63d',
+  dataField: ['original_title', 'original_title.search'],
+  url: 'https://scalr.api.appbase.io',
+  size: 5
 });
 
-searchbox('#git', { instance, openOnFocus: true }, [
-	{
-		templates: {
-			suggestion: function(suggestion) {
-				return `<p class="is-4">${suggestion.label}</p>`;
-			},
-			empty: function() {
-				return `<div>No Results</div>`;
-			},
-			loader: function() {
-				return `<div>Loader</div>`;
-			}
-		}
-	}
+instance.triggerQuery();
+
+const micButton = document.getElementById('voice');
+const input = document.getElementById('input');
+
+micButton.addEventListener('click', () => {
+  instance.onMicClick(null, {
+    triggerSuggestionsQuery: true
+  });
+});
+
+instance.onMicStatusChange = status => {
+  switch (status) {
+    case 'ACTIVE':
+      micButton.className = 'voice active';
+      break;
+    case 'DENIED':
+      micButton.className = 'voice disabled';
+      break;
+    default:
+      micButton.className = 'voice';
+  }
+};
+
+instance.onValueChange = value => {
+  input.value = value.toLowerCase();
+  micButton.className = 'voice';
+};
+
+searchbox('#input', { instance, openOnFocus: true }, [
+  {
+    templates: {
+      suggestion: function(suggestion) {
+        return `<p class="is-4">${suggestion.label}</p>`;
+      },
+      empty: function() {
+        return `<div>No Results</div>`;
+      },
+      loader: function() {
+        return `<div class="loader">Loading...</div>`;
+      }
+    }
+  }
 ]);
