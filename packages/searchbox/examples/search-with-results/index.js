dc1524753c37458ddf419140dde80415ddf70ebd
jyash97 <jyash97@gmail.com>
2020-08-25 17:07:46 +0530

    fix: examples creds and searchbox voice search

--- packages/searchbox/examples/search-with-results/index.js
+++ packages/searchbox/examples/search-with-results/index.js
@@ -2,25 +2,18 @@ import searchbox from '@appbaseio/searchbox';
 import Searchbase from '@appbaseio/searchbase';
 
 const instance = new Searchbase({
-	index: 'gitxplore-latest-app',
-	credentials: 'LsxvulCKp:a500b460-73ff-4882-8d34-9df8064b3b38',
-	url: 'https://scalr.api.appbase.io',
-	size: 5,
-	dataField: [
-		'name',
-		'description',
-		'name.raw',
-		'fullname',
-		'owner',
-		'topics'
-	]
+  index: 'gitxplore-app',
+  credentials: 'a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61',
+  url: 'https://appbase-demo-ansible-abxiydt-arc.searchbase.io',
+  size: 5,
+  dataField: ['name', 'description', 'name.raw', 'fullname', 'owner', 'topics']
 });
 
 const resultElement = document.getElementById('root');
 
 instance.onResults = ({ data }) => {
-	const resultMarkup = data.map(item => {
-		return `
+  const resultMarkup = data.map(item => {
+    return `
 			<div class="col-md-6 col-sm-12 my-2">
 				<div class="card">
 					<div class="row no-gutters">
@@ -37,49 +30,49 @@ instance.onResults = ({ data }) => {
 				</div>
 			</div>
 		`;
-	});
+  });
 
-	resultElement.innerHTML = resultMarkup.join('\n');
+  resultElement.innerHTML = resultMarkup.join('\n');
 };
 
 instance.onRequestStatusChange = status => {
-	if (status === 'PENDING') {
-		resultElement.innerHTML = `
+  if (status === 'PENDING') {
+    resultElement.innerHTML = `
 			<div class="spinner-border" role="status">
 				<span class="sr-only">Loading...</span>
 			</div>
 		`;
-	}
+  }
 };
 
 searchbox('#git', { instance }, [
-	{
-		templates: {
-			suggestion: function(suggestion) {
-				return `<p class="is-4">${suggestion.label}</p>`;
-			},
-			empty: function() {
-				return `<div>No Results</div>`;
-			},
-			loader: function() {
-				return `<div>Loader</div>`;
-			},
-			footer: function({ query, isEmpty }) {
-				return `
+  {
+    templates: {
+      suggestion: function(suggestion) {
+        return `<p class="is-4">${suggestion.label}</p>`;
+      },
+      empty: function() {
+        return `<div>No Results</div>`;
+      },
+      loader: function() {
+        return `<div>Loader</div>`;
+      },
+      footer: function({ query, isEmpty }) {
+        return `
                     <div style="background: #eaeaea; padding: 10px;">Footer</div>
                 `;
-			},
-			header: function({ query, isEmpty }) {
-				return `
+      },
+      header: function({ query, isEmpty }) {
+        return `
                     <div style="background: #efefef; padding: 10px;">
                         Hello From Header
                     </div>
                 `;
-			}
-		}
-	}
+      }
+    }
+  }
 ]).on('selected', function(event, suggestion) {
-	instance.setValue(suggestion.value, {
-		triggerQuery: true
-	});
+  instance.setValue(suggestion.value, {
+    triggerQuery: true
+  });
 });
--- packages/searchbox/examples/search/index.js
+++ packages/searchbox/examples/search/index.js
@@ -2,24 +2,17 @@ import searchbox from '@appbaseio/searchbox';
 import Searchbase from '@appbaseio/searchbase';
 
 const instance = new Searchbase({
-	index: 'gitxplore-latest-app',
-	credentials: 'LsxvulCKp:a500b460-73ff-4882-8d34-9df8064b3b38',
-	url: 'https://scalr.api.appbase.io',
-	size: 5,
-	dataField: [
-		'name',
-		'description',
-		'name.raw',
-		'fullname',
-		'owner',
-		'topics'
-	]
+  index: 'gitxplore-app',
+  credentials: 'a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61',
+  url: 'https://appbase-demo-ansible-abxiydt-arc.searchbase.io',
+  size: 5,
+  dataField: ['name', 'description', 'name.raw', 'fullname', 'owner', 'topics']
 });
 
 const querySuggestionsInstance = new Searchbase({
   index: 'good-books-clone',
-  url: 'https://arc-cluster-appbase-demo-6pjy6z.searchbase.io',
   credentials: 'a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61',
+  url: 'https://appbase-demo-ansible-abxiydt-arc.searchbase.io',
   size: 5,
   dataField: ['original_title', 'original_title.search'],
   enableQuerySuggestions: true,
@@ -28,240 +21,235 @@ const querySuggestionsInstance = new Searchbase({
 });
 
 const analyticsInstance = new Searchbase({
-	index: 'movies-store-app',
-	credentials: 'ctWRp9QBE:fece5752-b478-452b-8173-00b278e5e0b0',
-	url: 'https://scalr.api.appbase.io',
-	size: 5,
-	analytics: true,
-	dataField: [
-		'original_title',
-		'original_title.autosuggest',
-		'original_title.english',
-		'original_title.keyword',
-		'original_title.search'
-	]
+  index: 'movies-store-app',
+  credentials: 'a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61',
+  url: 'https://appbase-demo-ansible-abxiydt-arc.searchbase.io',
+  size: 5,
+  analytics: true,
+  dataField: [
+    'original_title',
+    'original_title.autosuggest',
+    'original_title.english',
+    'original_title.keyword',
+    'original_title.search'
+  ]
 });
 
 searchbox('#git', { instance }, [
-	{
-		templates: {
-			suggestion: function(suggestion) {
-				return `<p class="is-4">${suggestion.label}</p>`;
-			},
-			empty: function() {
-				return `<div>No Results</div>`;
-			},
-			loader: function() {
-				return `<div>Loader</div>`;
-			},
-			footer: function({ query, isEmpty }) {
-				return `
+  {
+    templates: {
+      suggestion: function(suggestion) {
+        return `<p class="is-4">${suggestion.label}</p>`;
+      },
+      empty: function() {
+        return `<div>No Results</div>`;
+      },
+      loader: function() {
+        return `<div>Loader</div>`;
+      },
+      footer: function({ query, isEmpty }) {
+        return `
                     <div style="background: #eaeaea; padding: 10px;">Footer</div>
                 `;
-			},
-			header: function({ query, isEmpty }) {
-				return `
+      },
+      header: function({ query, isEmpty }) {
+        return `
                     <div style="background: #efefef; padding: 10px;">
                         Hello From Header
                     </div>
                 `;
-			}
-		}
-	}
+      }
+    }
+  }
 ]);
 
 searchbox('#git2', { hint: false, instance }, [
-	{
-		templates: {
-			suggestion: function(suggestion) {
-				return `<p class="is-4">${suggestion.label}</p>`;
-			}
-		}
-	}
+  {
+    templates: {
+      suggestion: function(suggestion) {
+        return `<p class="is-4">${suggestion.label}</p>`;
+      }
+    }
+  }
 ]);
 
 searchbox('#git3', { autoselect: true, instance }, [
-	{
-		templates: {
-			suggestion: function(suggestion) {
-				return `<p class="is-4">${suggestion.label}</p>`;
-			}
-		}
-	}
+  {
+    templates: {
+      suggestion: function(suggestion) {
+        return `<p class="is-4">${suggestion.label}</p>`;
+      }
+    }
+  }
 ]);
 
 searchbox('#git4', { clearOnSelected: true, instance }, [
-	{
-		templates: {
-			suggestion: function(suggestion) {
-				return `<p class="is-4">${suggestion.label}</p>`;
-			}
-		}
-	}
+  {
+    templates: {
+      suggestion: function(suggestion) {
+        return `<p class="is-4">${suggestion.label}</p>`;
+      }
+    }
+  }
 ]);
 
 searchbox('#git5', { openOnFocus: true, instance }, [
-	{
-		templates: {
-			suggestion: function(suggestion) {
-				return `<p class="is-4">${suggestion.label}</p>`;
-			}
-		}
-	}
+  {
+    templates: {
+      suggestion: function(suggestion) {
+        return `<p class="is-4">${suggestion.label}</p>`;
+      }
+    }
+  }
 ]);
 
 searchbox('#git6', { keyboardShortcuts: ['s', 'a'], instance }, [
-	{
-		templates: {
-			suggestion: function(suggestion) {
-				return `<p class="is-4">${suggestion.label}</p>`;
-			}
-		}
-	}
+  {
+    templates: {
+      suggestion: function(suggestion) {
+        return `<p class="is-4">${suggestion.label}</p>`;
+      }
+    }
+  }
 ]);
 
 searchbox('#git7', { minLength: 3, instance }, [
-	{
-		templates: {
-			suggestion: function(suggestion) {
-				return `<p class="is-4">${suggestion.label}</p>`;
-			}
-		}
-	}
+  {
+    templates: {
+      suggestion: function(suggestion) {
+        return `<p class="is-4">${suggestion.label}</p>`;
+      }
+    }
+  }
 ]);
 
 searchbox('#git8', { instance }, [
-	{
-		templates: {
-			suggestion: function(suggestion) {
-				return `<p class="is-4">${suggestion.label}</p>`;
-			}
-		}
-	}
+  {
+    templates: {
+      suggestion: function(suggestion) {
+        return `<p class="is-4">${suggestion.label}</p>`;
+      }
+    }
+  }
 ])
-	.on('selected', function(event, suggestion, dataset, context) {
-		console.log('selected', suggestion, dataset, context);
-	})
-	.on('cursorchanged', function(event, suggestion, dataset) {
-		console.log('cursorchanged', suggestion, dataset);
-	})
-	.on('opened', function(event) {
-		console.log('opened', event);
-	})
-	.on('shown', function(event) {
-		console.log('shown', event);
-	})
-	.on('empty', function(event) {
-		console.log('empty', event);
-	});
+  .on('selected', function(event, suggestion, dataset, context) {
+    console.log('selected', suggestion, dataset, context);
+  })
+  .on('cursorchanged', function(event, suggestion, dataset) {
+    console.log('cursorchanged', suggestion, dataset);
+  })
+  .on('opened', function(event) {
+    console.log('opened', event);
+  })
+  .on('shown', function(event) {
+    console.log('shown', event);
+  })
+  .on('empty', function(event) {
+    console.log('empty', event);
+  });
 
 searchbox('#git9', { instance }, [
-	{
-		templates: {
-			suggestion: function(suggestion) {
-				return `<p class="is-4">${suggestion.label}</p>`;
-			}
-		},
-		debounce: 1000
-	}
+  {
+    templates: {
+      suggestion: function(suggestion) {
+        return `<p class="is-4">${suggestion.label}</p>`;
+      }
+    },
+    debounce: 1000
+  }
 ]);
 
 searchbox('#git10', { instance }, [
-	{
-		templates: {
-			suggestion: function(suggestion) {
-				return `<p class="is-4">${suggestion.label}</p>`;
-			}
-		},
-		displayKey: function(suggestion) {
-			return 'Awesome ' + suggestion.label + ' Blossom';
-		}
-	}
+  {
+    templates: {
+      suggestion: function(suggestion) {
+        return `<p class="is-4">${suggestion.label}</p>`;
+      }
+    },
+    displayKey: function(suggestion) {
+      return 'Awesome ' + suggestion.label + ' Blossom';
+    }
+  }
 ]);
 
 searchbox('#git11', { instance: analyticsInstance }, [
-	{
-		templates: {
-			suggestion: function(suggestion) {
-				return `<p class="is-4">${suggestion.label}</p>`;
-			}
-		}
-	}
+  {
+    templates: {
+      suggestion: function(suggestion) {
+        return `<p class="is-4">${suggestion.label}</p>`;
+      }
+    }
+  }
 ]);
 
 searchbox('#git12', { instance }, [
-	{
-		templates: {
-			suggestion: function(suggestion) {
-				return `<p class="is-4">${suggestion.label}</p>`;
-			},
-			loader: function() {
-				return `<p>Loading Things</p>`;
-			}
-		}
-	}
+  {
+    templates: {
+      suggestion: function(suggestion) {
+        return `<p class="is-4">${suggestion.label}</p>`;
+      },
+      loader: function() {
+        return `<p>Loading Things</p>`;
+      }
+    }
+  }
 ]);
 
 searchbox(
-	'#git13',
-	{
-		instance: analyticsInstance
-	},
-	[
-		{
-			templates: {
-				suggestion: function(suggestion) {
-					if (suggestion.source._promoted) {
-						return `<p class="is-4">${suggestion.label} <span class="badge badge-primary">Promoted</span></p>`;
-					}
-					return `<p class="is-4">${suggestion.label}</p>`;
-				}
-			}
-		}
-	]
+  '#git13',
+  {
+    instance: analyticsInstance
+  },
+  [
+    {
+      templates: {
+        suggestion: function(suggestion) {
+          if (suggestion.source._promoted) {
+            return `<p class="is-4">${suggestion.label} <span class="badge badge-primary">Promoted</span></p>`;
+          }
+          return `<p class="is-4">${suggestion.label}</p>`;
+        }
+      }
+    }
+  ]
 );
 
 searchbox(
-	'#git14',
-	{
-		instance: analyticsInstance
-	},
-	[
-		{
-			templates: {
-				render: function({
-					data,
-					getItemProps,
-					promotedData,
-					resultStats
-				}) {
-					const suggestionHTML = data
-						.filter(i => !i.source._promoted)
-						.reduce((agg, i) => {
-							return (
-								agg +
-								`
+  '#git14',
+  {
+    instance: analyticsInstance
+  },
+  [
+    {
+      templates: {
+        render: function({ data, getItemProps, promotedData, resultStats }) {
+          const suggestionHTML = data
+            .filter(i => !i.source._promoted)
+            .reduce((agg, i) => {
+              return (
+                agg +
+                `
 									<div ${getItemProps(i)}>
 										${i.label}
 									</div>
 								`
-							);
-						}, '');
-					const promotedHTML = data
-						.filter(i => !!i.source._promoted)
-						.reduce((agg, i) => {
-							return (
-								agg +
-								`
+              );
+            }, '');
+          const promotedHTML = data
+            .filter(i => !!i.source._promoted)
+            .reduce((agg, i) => {
+              return (
+                agg +
+                `
 									<div ${getItemProps(i)}>
 										${i.label}
 									</div>
 								`
-							);
-						}, '');
+              );
+            }, '');
 
-					if (resultStats.promoted) {
-						return `
+          if (resultStats.promoted) {
+            return `
 							<div class="shadow-sm p-2 text-light bg-primary">
 								Promoted Results
 							</div>
@@ -274,8 +262,8 @@ searchbox(
 								Found ${resultStats.numberOfResults} in ${resultStats.time}ms
 							</div>
 						`;
-					}
-					return `
+          }
+          return `
 						<div class="shadow-sm p-2 text-light bg-primary">
 							Search Results
 						</div>
@@ -284,10 +272,10 @@ searchbox(
 							Found ${resultStats.numberOfResults} in ${resultStats.time}ms
 						</div>
 					`;
-				}
-			}
-		}
-	]
+        }
+      }
+    }
+  ]
 );
 
 searchbox(
@@ -298,11 +286,7 @@ searchbox(
   [
     {
       templates: {
-        render: function({
-                           data,
-                           getItemProps,
-                           querySuggestions
-                         }) {
+        render: function({ data, getItemProps, querySuggestions }) {
           const suggestionHTML = (querySuggestions || [])
             .concat(data)
             .filter(i => !i.source._promoted)
@@ -336,10 +320,7 @@ searchbox(
   [
     {
       templates: {
-        render: function({
-                           data,
-                           getItemProps,
-                         }) {
+        render: function({ data, getItemProps }) {
           const suggestionHTML = data
             .filter(i => !i.source._promoted)
             .reduce((agg, i) => {
@@ -359,7 +340,7 @@ searchbox(
 						${suggestionHTML}
 					`;
         },
-        renderQuerySuggestions: function({ data, getItemProps}) {
+        renderQuerySuggestions: function({ data, getItemProps }) {
           const suggestionHTML = (data || [])
             .filter(i => !i.source._promoted)
             .reduce((agg, i) => {
@@ -372,7 +353,7 @@ searchbox(
 								`
               );
             }, '');
-          if ((data||[]).length === 0) return '';
+          if ((data || []).length === 0) return '';
           return `
 						<div class="shadow-sm p-2 text-light bg-primary">
 							Query Suggestion Results
