39c4c795a00b6e6da5b0183b0ec818ca56889156
bietkul <kuldepsaxena155@gmail.com>
2021-03-09 16:15:18 +0530

    feat: angular demo step-2

--- packages/searchbase/examples/with-angular/src/app/app.component.ts
+++ packages/searchbase/examples/with-angular/src/app/app.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
-import { SearchBase } from '@appbaseio/searchbase';
+import { Component, AfterContentInit } from '@angular/core';
+import { SearchBase, SearchComponent } from '@appbaseio/searchbase';
+import { of, Observable, from } from 'rxjs';
+import { distinctUntilChanged, switchMap, map } from 'rxjs/operators';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent  {
+export class AppComponent implements AfterContentInit  {
   index = 'gitxplore-app';
   url = 'https://appbase-demo-ansible-abxiydt-arc.searchbase.io';
   credentials = 'a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61';
 
 
   searchBase: SearchBase;
+
+  searchComponent: SearchComponent;
+  filterComponent: SearchComponent;
+  resultComponent: SearchComponent;
+  // A list of suggestions
+  suggestions: Observable<any[]>;
+
 
   constructor() {
     // Create searchbase instance
@@ -21,7 +32,87 @@ export class AppComponent  {
       url: this.url,
       credentials: this.credentials
     });
+
+    // Register search component => To render the auto-suggestions
+    this.searchComponent = this.searchBase.register('search-component', {
+      dataField: [
+       'name', 'description', 'name.raw', 'fullname', 'owner', 'topics'
+      ],
+      size: 5
+    });
+
+    // Register filter component with dependency on search component
+    this.filterComponent = this.searchBase.register('language-filter', {
+      type: 'term',
+      dataField: 'language.keyword',
+      aggregationSize: 10,
+      size: 0,
+      value: [],
+      react: {
+        and: ['search-component']
+      },
+    });
+
+    // Register result component with react dependency on search and filter components
+    this.resultComponent = this.searchBase.register('result-component', {
+      dataField: 'name',
+      react: {
+        and: ['search-component', 'language-filter']
+      },
+      from: 0,
+      size: 10,
+      defaultQuery: () => ({
+        track_total_hits: true
+      })
+    });
   }
 
+  ngAfterContentInit() {
+    // Fetch initial filter options
+    this.filterComponent.triggerDefaultQuery();
+    // Fetch initial results
+    this.resultComponent.triggerDefaultQuery();
+  }
 
+  // Method to set the suggestions
+	setSuggestions(value) {
+    // If value is empty then don't fetch suggestions
+    if(!value) {
+      this.searchComponent.setValue('', {
+        triggerDefaultQuery: false,
+        triggerCustomQuery: false,
+      });
+    } else {
+        // Update suggestions when value gets changed
+        this.suggestions = of(value).pipe(
+          distinctUntilChanged(),
+          switchMap(val => {
+            this.searchComponent.setValue(val, {
+              triggerDefaultQuery: false,
+              triggerCustomQuery: false,
+            });
+            return from(this.searchComponent.triggerDefaultQuery())
+            .pipe(
+              map(() => this.searchComponent.suggestions))
+          })
+        )
+      }
+  }
+
+  // To fetch the suggestions
+  handleInput(e) {
+    this.setSuggestions(e.target.value);
+  };
+
+  handleOptionSelect(selectedOption: MatAutocompleteSelectedEvent) {
+    this.searchComponent.setValue(selectedOption.option.value, {
+      triggerCustomQuery: true, // to update results
+      triggerDefaultQuery: true // to update suggestions
+    })
+  }
+
+  handleSelection() {
+    // Update results when language changes
+    this.filterComponent.triggerCustomQuery();
+  }
 }
--- packages/searchbase/examples/with-angular/src/app/app.module.ts
+++ packages/searchbase/examples/with-angular/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -12,6 +15,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     NoopAnimationsModule,
     NgbModule,
+    MatAutocompleteModule,
+    FormsModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
