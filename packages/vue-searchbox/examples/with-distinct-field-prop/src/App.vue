15b2ad626a0577c3a55ed3fb6fe12c7f90fb7226
Anik Ghosh <tech.anikghosh@gmail.com>
2021-04-30 14:05:35 +0530

    fix: distinct field example updated in vue searchbox

--- packages/vue-searchbox/examples/with-distinct-field-prop/src/App.vue
+++ packages/vue-searchbox/examples/with-distinct-field-prop/src/App.vue
@@ -39,128 +39,134 @@
           ]"
           title="Search"
           placeholder="Search for Books"
-          distinctField="authors.keyword"
-          :distinctFieldConfig="{
-              inner_hits: {
-                  name: 'most_recent',
-                  size: 5,
-                  sort: [{ timestamp: 'asc' }],
-              },
-              max_concurrent_group_searches: 4,
-          }"
         />
-        <div class="row">
-          <div class="col">
-            <search-component
-              id="author-filter"
-              type="term"
-              dataField="authors.keyword"
-              :subscribeTo="['aggregationData', 'requestStatus', 'value']"
-              :URLParams="true"
-              :react="{ and: ['search-component'] }"
-              :value="[]"
+        <div>
+          <search-component
+            id="result-component"
+            :dataField="['original_title']"
+            :size="5"
+            :react="{ and: ['search-component'] }"
+            distinctField="authors.keyword"
+            :distinctFieldConfig="{
+              inner_hits: {
+                name: 'other_books',
+                size: 3,
+                sort: [{ timestamp: 'asc' }]
+              },
+              max_concurrent_group_searches: 4
+            }"
+          >
+            <div
+              class="result-list-container"
+              slot-scope="{ loading, error, results, size, from, setFrom }"
             >
-              <div
-                class="filter-container"
-                slot-scope="{ aggregationData, loading, value, setValue }"
-              >
-                <div v-if="loading">Fetching Results ....</div>
-                <div v-if="!loading">
-                  <div
-                    v-bind:key="item._key"
-                    v-for="item in aggregationData.data"
-                  >
-                    <div class="list-item" key="{{item._key}}">
-                      <input
-                        type="checkbox"
-                        :value="item._key"
-                        :checked="value ? value.includes(item._key) : false"
-                        @change="handleChange($event, value, setValue)"
-                      />
-                      <label class="list-item-label" :htmlFor="item._key">
-                        {{ item._key }} ({{ item._doc_count }})
-                      </label>
-                    </div>
-                  </div>
-                </div>
+              <div v-if="loading">Fetching Results ....</div>
+              <div v-if="Boolean(error)">
+                Something went wrong! Error details
+                {{ JSON.stringify(error) }}
               </div>
-            </search-component>
-          </div>
-          <div class="col">
-            <search-component
-              id="result-component"
-              :dataField="['original_title']"
-              :size="5"
-              :react="{ and: ['search-component', 'author-filter'] }"
-            >
-              <div
-                class="result-list-container"
-                slot-scope="{ loading, error, results, size, from, setFrom }"
-              >
-                <div v-if="loading">Fetching Results ....</div>
-                <div v-if="Boolean(error)">
-                  Something went wrong! Error details
-                  {{ JSON.stringify(error) }}
-                </div>
-                <p v-if="!loading && !error">
-                  {{ results.numberOfResults }} results found in
-                  {{ results.time }}ms
-                </p>
-                <div v-if="!loading && !error">
-                  <div v-bind:key="item._id" v-for="item in results.data">
-                    <div class="flex book-content" key="item._id">
-                      <img
-                        :src="item.image"
-                        alt="Book Cover"
-                        class="book-image"
-                      />
-                      <div class="flex column justify-center ml20">
-                        <div class="book-header">{{ item.original_title }}</div>
-                        <div class="flex column justify-space-between">
+              <p v-if="!loading && !error">
+                {{ results.numberOfResults }} results found in
+                {{ results.time }}ms
+              </p>
+              <div v-if="!loading && !error">
+                <div v-bind:key="item._id" v-for="item in results.data">
+                  <div class="flex book-content" key="item._id">
+                    <img
+                      :src="item.image"
+                      alt="Book Cover"
+                      class="book-image"
+                    />
+                    <div class="flex column justify-center book-container">
+                      <div class="book-header">{{ item.original_title }}</div>
+                      <div class="flex column justify-space-between">
+                        <div>
                           <div>
-                            <div>
-                              by
-                              <span class="authors-list">{{
-                                item.authors
-                              }}</span>
-                            </div>
-                            <div class="ratings-list flex align-center">
-                              <span class="stars">
-                                <i
-                                  v-for="(item, index) in Array(
-                                    item.average_rating_rounded
-                                  ).fill('x')"
-                                  class="fas fa-star"
-                                  :key="index"
-                                />
-                              </span>
-                              <span class="avg-rating"
-                                >({{ item.average_rating }} avg)</span
-                              >
+                            by
+                            <span class="authors-list">{{ item.authors }}</span>
+                          </div>
+                          <div class="ratings-list flex align-center">
+                            <span class="stars">
+                              <i
+                                v-for="(item, index) in Array(
+                                  item.average_rating_rounded
+                                ).fill('x')"
+                                class="fas fa-star"
+                                :key="index"
+                              />
+                            </span>
+                            <span class="avg-rating"
+                              >({{ item.average_rating }} avg)</span
+                            >
+                            <span class="avg-rating"
+                              >({{
+                                item.inner_hits.other_books.hits.hits.length
+                              }}
+                              length)</span
+                            >
+                          </div>
+                        </div>
+                        <span class="pub-year"
+                          >Pub {{ item.original_publication_year }}</span
+                        >
+                      </div>
+                    </div>
+                    <div
+                      v-if="item.inner_hits.other_books.hits.hits.length > 1"
+                      class="flex other-books-container"
+                    >
+                      <div class="flex row other-books-header">
+                        <span class="pub-year">
+                          Other books by the same Author(s) :
+                        </span>
+                      </div>
+                      <div class="flex row other-books-sub-container">
+                        <div
+                          class="flex row"
+                          v-bind:key="book._id"
+                          v-for="book in filterOtherBooks(item)"
+                        >
+                          <!-- ------------------------------------------------ -->
+                          <div
+                            class="flex col"
+                            style="background-color: #ffffff; border: none;"
+                          >
+                            <div class="flex row">
+                              <img
+                                :src="
+                                  book._source.image_medium ||
+                                    book._source.image
+                                "
+                                alt="Book Cover"
+                                class="other-books-cover"
+                              />
+                              <div class="other-books-title-container">
+                                <h6 class="other-books-title">
+                                  {{ book._source.original_title }}
+                                </h6>
+                              </div>
                             </div>
                           </div>
-                          <span class="pub-year"
-                            >Pub {{ item.original_publication_year }}</span
-                          >
+                          <!-- ------------------------------------------------ -->
                         </div>
                       </div>
                     </div>
                   </div>
-                  <div class="pagination-wrapper">
-                    <paginate
-                      v-if="results.numberOfResults"
-                      :value="Math.ceil(from / size) + 1"
-                      :page-count="Math.ceil(results.numberOfResults / size)"
-                      :click-handler="page => setFrom((page - 1) * size)"
-                      :prev-text="'Prev'"
-                      :next-text="'Next'"
-                      :container-class="'pagination'"
-                    ></paginate>
-                  </div>
+                </div>
+                <div class="pagination-wrapper">
+                  <paginate
+                    v-if="results.numberOfResults"
+                    :value="Math.ceil(from / size) + 1"
+                    :page-count="Math.ceil(results.numberOfResults / size)"
+                    :click-handler="page => setFrom((page - 1) * size)"
+                    :prev-text="'Prev'"
+                    :next-text="'Next'"
+                    :container-class="'pagination'"
+                  ></paginate>
                 </div>
               </div>
-            </search-component>
-          </div>
+            </div>
+          </search-component>
         </div>
       </div>
     </search-base>
@@ -201,6 +207,14 @@ export default {
         triggerCustomQuery: true,
         stateChanges: true
       });
+    },
+    filterOtherBooks(item) {
+      const otherBooks = item.inner_hits
+        ? item.inner_hits.other_books.hits.hits
+        : [];
+      return otherBooks.filter(
+        book => book._source.original_title !== item.original_title
+      );
     }
   }
 };
