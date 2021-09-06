3fff1bf626ec953f5a48bec823329364d1eb1541
mohdashraf010897 <mohdashraf010897@gmail.com>
2021-09-06 16:00:56 +0530

    fix(react-native): transform-request-props-example

--- packages/native/examples/with-transform-request-prop/App.js
+++ packages/native/examples/with-transform-request-prop/App.js
@@ -16,7 +16,6 @@ import {
   Image,
   SafeAreaView
 } from 'react-native';
-import axios from 'axios';
 
 const renderResultItem = ({ item }) => {
   return (
@@ -57,9 +56,11 @@ const renderItemSeparator = () => {
   );
 };
 
-export default function App() {
+function AppContent() {
   const [dataSource, setDataSource] = useState([]);
   const [resetPagination, setResetPagination] = useState(false);
+  const [queryVal, setQueryVal] = useState('');
+
   const stateRef = useRef();
   stateRef.current = dataSource;
   const stateRefQuery = useRef();
@@ -75,125 +76,144 @@ export default function App() {
   };
   return (
     <SafeAreaView style={styles.container}>
-      <SearchBase
-        index="good-books-ds"
-        credentials="a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61"
-        url="https://appbase-demo-ansible-abxiydt-arc.searchbase.io"
-        appbaseConfig={{
-          recordAnalytics: true,
-          enableQueryRules: true,
-          userId: 'jon@appbase.io',
-          customEvents: {
-            platform: 'ios',
-            device: 'iphoneX'
+      <SearchBox
+        id="search-component"
+        dataField={[
+          {
+            field: 'original_title',
+            weight: 1
+          },
+          {
+            field: 'original_title.search',
+            weight: 3
           }
+        ]}
+        onValueSelected={value => {
+          setResetPagination(true);
         }}
-      >
-        <SearchBox
-          id="search-component"
-          dataField={[
-            {
-              field: 'original_title',
-              weight: 1
-            },
-            {
-              field: 'original_title.search',
-              weight: 3
+        transformRequest={request => {
+          const suggestedWordsList = [];
+          let reqBody = JSON.parse(request.body);
+          let getSearchComponentQueryIndex = 0;
+          reqBody.query.forEach((item, index) => {
+            if (item.id === 'search-component') {
+              getSearchComponentQueryIndex = index;
             }
-          ]}
-          onValueSelected={value => {
-            setResetPagination(true);
-          }}
-          transformRequest={request => {
-            const suggestedWordsList = [];
-            let reqBody = JSON.parse(request.body);
-            let url =
-              'https://api.datamuse.com/words?sp=' +
-              reqBody.query[0].value +
-              '&max=2';
-            return (
-              axios
-                .get(url)
-                .then(({ data }) => {
-                  if (data.length > 0) {
-                    suggestedWordsList.push(data[0].word);
-                  }
-                  if (suggestedWordsList.length) {
-                    reqBody.query[0].value = suggestedWordsList[0];
-                  }
-                  let newRequest = {
-                    ...request,
-                    body: JSON.stringify(reqBody)
-                  };
-                  return Promise.resolve(newRequest);
-                })
-                .catch(err => console.error(err))
-                // eslint-disable-next-line
+          });
+          let queryWord = reqBody.query[getSearchComponentQueryIndex].value;
+          let url =
+            'https://api.datamuse.com/words?sp=' +
+            reqBody.query[getSearchComponentQueryIndex].value +
+            '&max=2';
+          return (
+            fetch(url)
+              .then(res => res.json())
+              .then(data => {
+                if (data.length > 0) {
+                  suggestedWordsList.push(data[0].word);
+                  queryWord = suggestedWordsList[0];
+                }
+                if (suggestedWordsList.length) {
+                  reqBody.query[getSearchComponentQueryIndex].value =
+                    suggestedWordsList[0];
+                }
+                let newRequest = {
+                  ...request,
+                  body: JSON.stringify(reqBody)
+                };
+                return Promise.resolve(newRequest);
+              })
+              .catch(err => console.error(err))
+              // eslint-disable-next-line
               .finally(() => {
-                  if (!suggestedWordsList.length) {
-                    return Promise.resolve(request);
-                  }
-                })
-            );
-          }}
-        />
-        <SearchComponent
-          id="result-component"
-          dataField="original_title"
-          size={10}
-          react={{
-            and: ['search-component']
-          }}
-          onResults={setResults}
-        >
-          {({ results, loading, size, from, setValue, setFrom }) => {
-            return (
-              <View>
-                {loading && !results.data.length ? (
-                  <ActivityIndicator
-                    style={styles.loader}
-                    size="large"
-                    color="#000"
-                  />
-                ) : (
-                  <View>
-                    {!results.data.length ? (
-                      <Text style={styles.resultStats}>No results found</Text>
-                    ) : (
-                      <View style={styles.resultContainer}>
-                        <Text style={styles.resultStats}>
-                          {results.numberOfResults} results found in{' '}
-                          {results.time}ms
-                        </Text>
-                        <FlatList
-                          data={dataSource}
-                          keyboardShouldPersistTaps={'handled'}
-                          keyExtractor={item => item._id}
-                          ItemSeparatorComponent={renderItemSeparator}
-                          renderItem={renderResultItem}
-                          onEndReached={() => {
-                            const offset = (from || 0) + size;
-                            if (results.numberOfResults > offset) {
-                              setFrom((from || 0) + size);
-                            }
-                          }}
-                          onEndReachedThreshold={0.5}
-                          ListFooterComponent={
-                            loading ? (
-                              <ActivityIndicator size="large" color="#000" />
-                            ) : null
+                setQueryVal(
+                  !queryWord || queryWord === 'undefined' ? '' : queryWord
+                );
+                if (!suggestedWordsList.length) {
+                  return Promise.resolve(request);
+                }
+              })
+          );
+        }}
+      />
+      <Text> {queryVal && <Text> Showing results for {queryVal}</Text>} </Text>
+      <SearchComponent
+        id="result-component"
+        dataField="original_title"
+        size={10}
+        react={{
+          and: ['search-component']
+        }}
+        onResults={setResults}
+      >
+        {({ results, loading, size, from, setValue, setFrom }) => {
+          return (
+            <View>
+              {loading && !results.data.length ? (
+                <ActivityIndicator
+                  style={styles.loader}
+                  size="large"
+                  color="#000"
+                />
+              ) : (
+                <View>
+                  {!results.data.length ? (
+                    <Text style={styles.resultStats}>No results found</Text>
+                  ) : (
+                    <View style={styles.resultContainer}>
+                      <Text style={styles.resultStats}>
+                        {results.numberOfResults} results found in{' '}
+                        {results.time}ms
+                      </Text>
+                      <FlatList
+                        data={dataSource}
+                        keyboardShouldPersistTaps={'handled'}
+                        keyExtractor={item => item._id}
+                        ItemSeparatorComponent={renderItemSeparator}
+                        renderItem={renderResultItem}
+                        onEndReached={() => {
+                          const offset = (from || 0) + size;
+                          if (results.numberOfResults > offset) {
+                            setFrom((from || 0) + size);
                           }
-                        />
-                      </View>
-                    )}
-                  </View>
-                )}
-              </View>
-            );
-          }}
-        </SearchComponent>
-      </SearchBase>
+                        }}
+                        onEndReachedThreshold={0.5}
+                        ListFooterComponent={
+                          loading ? (
+                            <ActivityIndicator size="large" color="#000" />
+                          ) : null
+                        }
+                      />
+                    </View>
+                  )}
+                </View>
+              )}
+            </View>
+          );
+        }}
+      </SearchComponent>
     </SafeAreaView>
+  );
+}
+
+export default function App() {
+  return (
+    <SearchBase
+      index="good-books-ds"
+      credentials="a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61"
+      url="https://appbase-demo-ansible-abxiydt-arc.searchbase.io"
+      appbaseConfig={{
+        recordAnalytics: true,
+        enableQueryRules: true,
+        userId: 'jon@appbase.io',
+        customEvents: {
+          platform: 'ios',
+          device: 'iphoneX'
+        }
+      }}
+    >
+      <AppContent />
+    </SearchBase>
   );
 }
 
