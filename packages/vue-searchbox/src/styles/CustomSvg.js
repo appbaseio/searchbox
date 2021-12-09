7332a9d14358fd13f7d718a9f67bbb0a99122373
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2021-12-10 02:56:15 +0530

    fix(vue-searchbox): fix search icon alignment issues

--- packages/vue-searchbox/src/styles/CustomSvg.js
+++ packages/vue-searchbox/src/styles/CustomSvg.js
@@ -1,3 +1,5 @@
+import SearchSvg from './SearchSvg';
+
 const CustomSvg = {
 	name: 'CustomSvg',
 	props: {
@@ -48,7 +50,6 @@ const CustomSvg = {
 				</svg>
 			);
 		}
-		window.console.log('this.$props.type', this.$props.type);
 		if (this.$props.type === 'popular-search-icon') {
 			return (
 				<svg
@@ -66,20 +67,16 @@ const CustomSvg = {
 			);
 		}
 		return (
-			<svg
-				alt="Search"
-				className="search-icon"
-				height="15"
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 15 15"
-				style={{
-					position: 'relative',
-					fill: '#707070'
+			<SearchSvg
+				props={{
+					style: {
+						position: 'relative',
+						fill: '#707070',
+						left: '3px',
+						marginRight: '8px'
+					}
 				}}
-			>
-				<title>Search</title>
-				<path d="M6.02945,10.20327a4.17382,4.17382,0,1,1,4.17382-4.17382A4.15609,4.15609,0,0,1,6.02945,10.20327Zm9.69195,4.2199L10.8989,9.59979A5.88021,5.88021,0,0,0,12.058,6.02856,6.00467,6.00467,0,1,0,9.59979,10.8989l4.82338,4.82338a.89729.89729,0,0,0,1.29912,0,.89749.89749,0,0,0-.00087-1.29909Z" />
-			</svg>
+			/>
 		);
 	}
 };
