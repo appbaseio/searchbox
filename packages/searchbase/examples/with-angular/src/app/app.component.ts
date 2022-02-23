08067912b05ada19f1b423fc9700e0be5df96964
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-02-23 23:36:02 +0530

    fix: set suggestions and language filter value type

--- packages/searchbase/examples/with-angular/src/app/app.component.ts
+++ packages/searchbase/examples/with-angular/src/app/app.component.ts
@@ -9,8 +9,7 @@ import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
-export class AppComponent implements AfterContentInit  {
+export class AppComponent implements AfterContentInit {
   index = 'gitxplore-app';
   url = 'https://appbase-demo-ansible-abxiydt-arc.searchbase.io';
   credentials = 'a03a1cb71321:75b6603d-9456-4a5a-af6b-a487b309eb61';
@@ -25,7 +24,6 @@ export class AppComponent implements AfterContentInit  {
   resultComponent: SearchComponent;
   // A list of suggestions
   suggestions: Observable<any[]>;
-
 
   constructor() {
     // Create searchbase instance
@@ -87,10 +85,17 @@ export class AppComponent implements AfterContentInit  {
           weight: 0.1
         }
       ],
-      includeFields: ['name', 'description', 'owner', 'fullname', 'language', 'topics'],
+      includeFields: [
+        'name',
+        'description',
+        'owner',
+        'fullname',
+        'language',
+        'topics'
+      ],
       size: 5,
       // To clear the filter values when search query gets changed
-      clearFiltersOnQueryChange: true,
+      clearFiltersOnQueryChange: true
     });
 
     // Register a component to filter languages with empty value
@@ -103,24 +108,24 @@ export class AppComponent implements AfterContentInit  {
             must_not: [
               {
                 match: {
-                  'language.keyword': ""
+                  'language.keyword': ''
                 }
               }
             ]
           }
-        },
+        }
       })
-    })
+    });
     // Register filter component with dependency on search component
     this.filterComponent = this.searchBase.register('language-filter', {
       type: 'term',
       dataField: 'language.keyword',
       aggregationSize: 10,
       size: 0,
-      value: [],
+      value: '',
       react: {
         and: ['search-component', 'filter-languages']
-      },
+      }
     });
 
     // Register result component with react dependency on search and filter components
@@ -148,43 +153,53 @@ export class AppComponent implements AfterContentInit  {
   }
 
   // Method to set the suggestions
-	setSuggestions(value) {
-    // If value is empty then don't fetch suggestions
-    if(!value) {
+  setSuggestions(value) {
+    if (!value) {
       this.searchComponent.setValue('', {
-        triggerDefaultQuery: false,
-        triggerCustomQuery: false,
+        triggerDefaultQuery: true,
+        triggerCustomQuery: true
       });
-      // clear suggestions
-      this.searchComponent.clearResults();
       this.suggestions = of(this.searchComponent.suggestions);
     } else {
-        // Update suggestions when value gets changed
-        this.suggestions = of(value).pipe(
-          distinctUntilChanged(),
-          switchMap(val => {
-            this.searchComponent.setValue(val, {
-              triggerDefaultQuery: false,
-              triggerCustomQuery: false,
-            });
-            return from(this.searchComponent.triggerDefaultQuery())
-            .pipe(
-              map(() => this.searchComponent.suggestions))
-          })
-        )
-      }
+      // Update suggestions when value gets changed
+      this.suggestions = of(value).pipe(
+        distinctUntilChanged(),
+        switchMap(val => {
+          this.searchComponent.setValue(val, {
+            triggerDefaultQuery: false,
+            triggerCustomQuery: false
+          });
+          return from(this.searchComponent.triggerDefaultQuery()).pipe(
+            map(() => this.searchComponent.suggestions)
+          );
+        })
+      );
+    }
+    // Update suggestions when value gets changed
+    this.suggestions = of(value).pipe(
+      distinctUntilChanged(),
+      switchMap(val => {
+        this.searchComponent.setValue(val, {
+          triggerDefaultQuery: false,
+          triggerCustomQuery: false
+        });
+        return from(this.searchComponent.triggerDefaultQuery()).pipe(
+          map(() => this.searchComponent.suggestions)
+        );
+      })
+    );
   }
 
   // To fetch the suggestions
   handleInput(e) {
     this.setSuggestions(e.target.value);
-  };
+  }
 
   handleOptionSelect(selectedOption: MatAutocompleteSelectedEvent) {
     this.searchComponent.setValue(selectedOption.option.value, {
       triggerCustomQuery: true, // to update results
       triggerDefaultQuery: true // to update suggestions
-    })
+    });
   }
 
   handleSelection() {
