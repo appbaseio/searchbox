37606d2573c2faceea64a011bca5ed58f7d90470
Shubham Shah <shubhamshahrising@gmail.com>
2022-04-15 10:20:11 +0530

    Test input

--- packages/searchbase/tests/index.test.js
+++ packages/searchbase/tests/index.test.js
@@ -6,8 +6,12 @@ const {
 const index = 'gitxplore-latest-app';
 const url = 'https://scalr.api.appbase.io';
 const credentials = 'LsxvulCKp:a500b460-73ff-4882-8d34-9df8064b3b38';
+const mongodb = {
+  db: 'sample_airbnb',
+  collection: 'listingsAndReviews'
+};
 
-describe('SearchBase instance: Error reporting', () => {
+describe('SearchBase: Input Validations (Elasticsearch as SearchBackend)', () => {
   test('throw error if empty url', () => {
     try {
       /* eslint-disable-next-line */
@@ -34,8 +38,37 @@ describe('SearchBase instance: Error reporting', () => {
       );
     }
   });
+  test('throw error if empty credentials', () => {
+    try {
+      /* eslint-disable-next-line */
+      const searchbase = new SearchBase({
+        index,
+        url: 'https://appbase-demo.dev.io'
+      });
+      expect(true).toBe(false);
+    } catch (e) {
+      expect(e.message).toEqual(
+        'credentials is required for <SearchBase /> component when used with the elasticsearch Search backend.'
+      );
+    }
+  });
 });
 
+describe('SearchBase: Input Validations (MongoDB as SearchBackend)', () => {
+  test('throw error if empty url', () => {
+    try {
+      /* eslint-disable-next-line */
+      const searchbase = new SearchBase({
+        mongodb
+      });
+      expect(true).toBe(false);
+    } catch (e) {
+      expect(e.message).toEqual(
+        'url is required for <SearchBase /> component when used with the mongodb Search backend.'
+      );
+    }
+  });
+});
 describe('SearchComponent instance: inheritance testing', () => {
   test('component credentials (index, url, credentials)', () => {
     /* eslint-disable-next-line */
