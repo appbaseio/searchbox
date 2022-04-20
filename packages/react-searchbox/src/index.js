3d82047dc583f88a8045f25bdf1ec1592a3ba6b2
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-04-21 00:43:14 +0530

    expose SearchBox component without wrapper

--- packages/react-searchbox/src/index.js
+++ packages/react-searchbox/src/index.js
@@ -1,39 +1,11 @@
-import React from 'react';
-import SearchBoxDefault from './components/SearchBox';
+import SearchBox from './components/SearchBox';
 import SearchBase from './components/SearchBase';
 import SearchComponent from './components/SearchComponent';
 import { SearchContext, getServerResults } from './utils/helper';
-import { Global, css } from '@emotion/core';
-
-class SearchBoxWithStyle extends React.Component {
-  state = { mount: false };
-
-  componentDidMount() {
-    this.setState({ mount: true });
-  }
-
-  render() {
-    return (
-      <div>
-        <Global
-          styles={css`
-            * {
-              margin: 0;
-              font-family: inherit;
-              -webkit-font-smoothing: antialiased;
-              -moz-osx-font-smoothing: grayscale;
-            }
-          `}
-        />
-        {this.state.mount && <SearchBoxDefault {...this.props} />}
-      </div>
-    );
-  }
-}
 
 export {
   SearchContext,
-  SearchBoxWithStyle as SearchBox,
+  SearchBox,
   SearchBase,
   SearchComponent,
   getServerResults
