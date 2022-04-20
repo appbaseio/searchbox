855d66a19c560ccf7f47ce857891e99d6c511fa9
Shubham Shah <shubhamshahrising@gmail.com>
2022-04-20 11:02:34 +0530

    Refactor to use table

--- packages/searchbase/tests/setters.test.js
+++ packages/searchbase/tests/setters.test.js
@@ -7,49 +7,47 @@ const index = 'gitxplore-latest-app';
 const url = 'https://scalr.api.appbase.io';
 const credentials = 'LsxvulCKp:a500b460-73ff-4882-8d34-9df8064b3b38';
 
+const query = {
+  query: {
+    match: {
+      name: 'harry'
+    }
+  }
+};
+
+// field,  initialValue, updateValue
+const settersTable = [
+  ['dataField', 'name', ['name', 'repositories']],
+  ['value', '', 'house'],
+  ['size', 5, 10],
+  ['from', 1, 2],
+  ['includeFields', ['name'], ['name', 'description']],
+  ['excludeFields', ['name'], ['name', 'description']],
+  ['sortBy', 'asc', 'desc'],
+  ['react', {}, { and: ['date-component'] }],
+  ['fuzziness', '0', 3],
+  ['after', {}, { date: '2015-10-01T06:00:00.000Z' }],
+  ['defaultQuery', null, query],
+  ['customQuery', null, query]
+];
+
 describe('SearchComponent: setter methods', () => {
-  test('should set dataField', () => {
+  test.each(settersTable)('should set %s', (field, initialVal, updatedVal) => {
     const searchBase = new SearchBase({ index, url, credentials });
     const componentId = 'search-component';
-    const nextDataFields = ['name', 'repositories'];
+    const capitalizedField = field[0].toUpperCase() + field.slice(1);
+    const setterName = 'set' + capitalizedField;
     const searchComponent = searchBase.register(componentId, {
-      dataField: ['name']
+      [field]: initialVal
     });
+
     searchComponent.subscribeToStateChanges(change => {
-      const dataFieldChange = change.dataField.next;
-      expect(dataFieldChange).toEqual(expect.arrayContaining(nextDataFields));
+      const fieldChange = change[field].next;
+      if (Array.isArray(updatedVal))
+        expect(fieldChange).toEqual(expect.arrayContaining(updatedVal));
+      else expect(fieldChange).toEqual(updatedVal);
     });
-    searchComponent.setDataField(nextDataFields, {
-      stateChanges: true
-    });
-  });
-  test('should set value', () => {
-    const searchBase = new SearchBase({ index, url, credentials });
-    const componentId = 'search-component';
-    const nextValue = 'House';
-    const searchComponent = searchBase.register(componentId, {
-      value: ''
-    });
-    searchComponent.subscribeToStateChanges(change => {
-      const valueChange = change.value.next;
-      expect(valueChange).toBe(nextValue);
-    });
-    searchComponent.setValue(nextValue, {
-      stateChanges: true
-    });
-  });
-  test('should set size', () => {
-    const searchBase = new SearchBase({ index, url, credentials });
-    const componentId = 'search-component';
-    const nextSize = 10;
-    const searchComponent = searchBase.register(componentId, {
-      size: 5
-    });
-    searchComponent.subscribeToStateChanges(change => {
-      const sizeChange = change.size.next;
-      expect(sizeChange).toBe(nextSize);
-    });
-    searchComponent.setSize(nextSize, {
+    searchComponent[setterName](updatedVal, {
       stateChanges: true
     });
   });
