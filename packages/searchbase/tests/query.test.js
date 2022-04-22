f565ea36657e5db6302a1ac02477a23e71c366a7
Shubham Shah <shubhamshahrising@gmail.com>
2022-04-22 17:06:48 +0530

    Complete request generation tests

--- packages/searchbase/tests/query.test.js
+++ packages/searchbase/tests/query.test.js
@@ -1,8 +1,13 @@
 const { SearchBase } = require('../dist/@appbaseio/searchbase.cjs.js');
 
+const suffix = '_reactivesearch.v3';
 const index = 'gitxplore-latest-app';
 const url = 'https://scalr.api.appbase.io';
 const credentials = 'LsxvulCKp:a500b460-73ff-4882-8d34-9df8064b3b38';
+const mongodb = {
+  db: 'sample_airbnb',
+  collection: 'listingsAndReviews'
+};
 
 beforeAll(() => jest.spyOn(window, 'fetch'));
 
@@ -15,7 +20,6 @@ describe('Request Generation Logic(Elasticsearch as backend)', () => {
       credentials
     });
   });
-
   test('should trigger query with all options', async () => {
     const componentId = 'search-component';
     const query = {
@@ -133,5 +137,103 @@ describe('Request Generation Logic(Elasticsearch as backend)', () => {
     expect(resultComponent.query[1]).toEqual(
       expect.objectContaining({ value: 'harry' })
     );
+  });
+
+  test('should trigger custom query as Elasticsearch query DSL', async () => {
+    const searchComponentId = 'search-component';
+    const resultComponentId = 'result-component';
+    const query = { query: { match: { title: 'harry' } } };
+    const searchComponent = searchBase.register(searchComponentId, {
+      customQuery: () => query
+    });
+    const resultComponent = searchBase.register(resultComponentId, {
+      react: { and: searchComponentId }
+    });
+
+    window.fetch.mockResolvedValueOnce({
+      ok: true,
+      json: async () => ({
+        [searchComponentId]: { hits: { hits: [] } },
+        [resultComponentId]: { hits: { hits: [] } }
+      })
+    });
+
+    await searchComponent.triggerCustomQuery();
+    expect(resultComponent.query[1].customQuery).toEqual(query);
+  });
+
+  test('should trigger custom query as stored query', async () => {
+    const searchComponentId = 'search-component';
+    const resultComponentId = 'result-component';
+    const query = { id: 'stored-query', params: { value: 'harry' } };
+    const searchComponent = searchBase.register(searchComponentId, {
+      customQuery: () => query
+    });
+    const resultComponent = searchBase.register(resultComponentId, {
+      react: { and: searchComponentId }
+    });
+
+    window.fetch.mockResolvedValueOnce({
+      ok: true,
+      json: async () => ({
+        [searchComponentId]: { hits: { hits: [] } },
+        [resultComponentId]: { hits: { hits: [] } }
+      })
+    });
+
+    await searchComponent.triggerCustomQuery();
+    expect(resultComponent.query[1].customQuery).toEqual(query);
+  });
+});
+describe('Request Generation Logic(MongoDB as SearchBackend', () => {
+  let searchBase;
+  beforeEach(() => {
+    searchBase = new SearchBase({
+      index,
+      url,
+      credentials,
+      mongodb
+    });
+  });
+  test('should trigger default query as MongoDB aggregation query', async () => {
+    const componentId = 'search-component';
+    const query = {
+      react: {
+        and: ['date-component', 'language-filter']
+      },
+      dataField: ['name', 'description'],
+      queryFormat: 'and'
+    };
+    const searchComponent = searchBase.register(componentId, {
+      ...query,
+      type: 'search'
+    });
+
+    window.fetch.mockResolvedValueOnce({
+      ok: true,
+      json: async () => ({
+        [componentId]: { hits: { hits: [] } }
+      })
+    });
+
+    await searchComponent.triggerDefaultQuery();
+    expect(searchComponent.query[0]).toEqual(expect.objectContaining(query));
+  });
+  test('should trigger custom query as MongoDB aggregation query', async () => {
+    const componentId = 'search-component';
+    const query = { query: { match: { title: 'harry' } } };
+    const searchComponent = searchBase.register(componentId, {
+      customQuery: () => query
+    });
+
+    window.fetch.mockResolvedValueOnce({
+      ok: true,
+      json: async () => ({
+        [componentId]: { hits: { hits: [] } }
+      })
+    });
+
+    await searchComponent.triggerCustomQuery();
+    expect(searchComponent.query[0]).toEqual(expect.objectContaining(query));
   });
 });
