df7f007dbb264cd0bf78370bf7b29e6db363163f
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2023-10-05 10:37:06 +0530

    correctly import createRoot func

--- packages/react-searchbox/examples/by-usecases/facet-filters/src/index.js
+++ packages/react-searchbox/examples/by-usecases/facet-filters/src/index.js
@@ -1,4 +1,4 @@
-import { createRoot } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import React from 'react';
 
 import App from './App';
--- packages/react-searchbox/examples/with-predictive-suggestions/src/index.js
+++ packages/react-searchbox/examples/with-predictive-suggestions/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createRoot } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const container = document.getElementById('root');
