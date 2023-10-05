4be291812b3d860b0681c127ce5c7503139a1fd3
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2023-10-05 10:22:38 +0530

    suport react 18 with examples update

--- packages/react-searchbox/examples/with-transform-request-prop/src/index.js
+++ packages/react-searchbox/examples/with-transform-request-prop/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom';
 
 import App from './App';
 
 import './styles.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
